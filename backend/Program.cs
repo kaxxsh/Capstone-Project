6e2b0f2233c5406a113cf4a68c104aec373cb69e
kaxxsh <kameshwaran2002@gmail.com>
2024-07-23 10:21:26 +0530

    first commit

---
+++ backend/Program.cs
@@ -0,0 +1,39 @@
+using backend.Middleware;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region Logger
+var logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .MinimumLevel.Information()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+#endregion
+
+#region ConfigureServices
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+#endregion
+
+var app = builder.Build();
+
+#region Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionMiddleware>();
+#endregion
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
